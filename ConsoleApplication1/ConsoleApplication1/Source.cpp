d05f3cbbfc74c8fb3bf5ad886adec8071ca5790f
Johnny Odabashyan <jodabash39@gmail.com>
2017-04-20 17:34:38 -0700

    fourth

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -5,5 +5,6 @@ int main()
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "I added more." << std::endl;
 	std::cout << "It appears that I have found a way to commit." << std::endl;
+	std::cout << "Hope it is still working." << std::endl;
 	return 0;
 }
